5148b57477aebb971d72be5f2b98f168b16c00ba
Dokho <palbabyshark@gmail.com>
2023-08-31 16:18:50 +0100

    up,right,left done

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-function Soap ( ) {
-    
+//PlAYER FUNCTION 
 
-    this.draw = function() {
+function Soap() {
+    var self = this
+    this.x = 11
+    this.y = 20
+    this.direction = function () {
+
+
+    }
+
+
+    this.draw = function () {
         // var soapCell = document.getElementsByClassName('row20')[0].getElementsByClassName('column11')[0];
         // console.log(soapCell)
         // soapCell.classList.add("player")
         var soapCell = document.querySelector('.row20 .column11')
-     soapCell.classList.add("soap")
+        soapCell.classList.add("soap")
+    }
+    //COMANDOS DEL JUEGO 
+
+    this.move = function (e) {
+        var key = e.key;
+
+        switch (e.key) {
+            case "w":
+                var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+
+                soapCell.classList.remove('soap')
+                self.y--
+
+                soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+                soapCell.classList.add("soap")
+                if (self.y ===1) {
+
+                    alert('YOU WIN!')
+ 
+
+                
+                }
+
+
+                break;
+            case "a":
+                var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+
+                soapCell.classList.remove('soap')
+
+                self.x--
+                soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+                soapCell.classList.add("soap")
+                break;
+            case "d":
+                var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+
+                soapCell.classList.remove('soap')
+
+                self.x++
+                soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
+                soapCell.classList.add("soap")
+                break;
+
+
+        }
     }
 }
- var objSoap = new Soap()
+
+
+
+var objSoap = new Soap()
 objSoap.draw()
+
+
+window.addEventListener("keydown", objSoap.move)
