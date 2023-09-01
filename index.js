6823cd10067d78ffd00811f422857913e2e27eb6
Dokho <palbabyshark@gmail.com>
2023-09-01 09:21:29 +0100

    collision with borders

--- index.js
+++ index.js
@@ -1,75 +1,54 @@
 import { Obstacles } from "./obstacles.js"
 
-
-
-
-//PlAYER FUNCTION 
-
 function Soap() {
     var self = this
     this.x = 11
     this.y = 20
     this.direction = function () {
 
-
     }
 
-
     this.draw = function () {
-        // var soapCell = document.getElementsByClassName('row20')[0].getElementsByClassName('column11')[0];
-        // console.log(soapCell)
-        // soapCell.classList.add("player")
         var soapCell = document.querySelector('.row20 .column11')
         soapCell.classList.add("soap")
     }
-    //COMANDOS DEL JUEGO 
 
-    this.move = function (e) {
+        this.move = function (e) {
         var key = e.key;
-
-        switch (e.key) {
+        switch (key) {
             case "w":
+                if (self.y > 1){
                 var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
-
                 soapCell.classList.remove('soap')
                 self.y--
-
                 soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
                 soapCell.classList.add("soap")
-                if (self.y ===1) {
-
+                if (self.y === 1) {
                     alert('YOU WIN!')
- 
-                
                 }
-
-
+            }
                 break;
             case "a":
+                if(self.x > 1){
                 var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
-
                 soapCell.classList.remove('soap')
-
                 self.x--
                 soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
                 soapCell.classList.add("soap")
+            }    
                 break;
             case "d":
+                if(self.x < 21){
                 var soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
-
                 soapCell.classList.remove('soap')
-
                 self.x++
                 soapCell = document.querySelector(`.row${self.y} .column${self.x}`)
                 soapCell.classList.add("soap")
+             }   
                 break;
-
-
         }
     }
 }
-
-
 
 var objSoap = new Soap()
 objSoap.draw()
--- obstacles.js
+++ obstacles.js
@@ -1,13 +1,13 @@
 function Obstacles(){
     this.x = x;
     this.y = y;
-    this.coords =
-    this.direcction =
-    this.insertObstacle =
-    this.removwObstacle =
+    this.coords
+    this.direcction
+    this.insertObstacle
+    this.removwObstacle
 }
 
 
-export {Obstacles}
+export { Obstacles }
 
 
